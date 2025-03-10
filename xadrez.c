8c5c65294c7bb7c404c340aae56ce9b22a9ecf93
Thiago Borghardt <thiago.mobdev@gmail.com>
2025-03-10 04:15:50 +0000

    Desafio do Novato. Usando os Loops FOR, WHILE e DOWHILE

--- xadrez.c
+++ xadrez.c
@@ -7,15 +7,35 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    const int TORRE_CASAS = 5;    // 5 casas para a direita
+    const int BISPO_CASAS = 5;    // 5 casas diagonal cima e direita
+    const int RAINHA_CASAS = 8;   // 8 casas para a esquerda
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("Movimento do BISPO:\n");
+    int contador_bispo = 0;
+    while(contador_bispo <= BISPO_CASAS){
+        printf("Cima e Direita\n");
+        contador_bispo++;
+    }
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("Movimento da Torre: \n");
+    for(int i = 1; i <= TORRE_CASAS; i++){
+        printf("Direita!\n");
+    }
+    printf("\n");
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("Movimento da Rainha:\n");
+    int contador_rainha = 0;
+    do{
+        printf("Esquerda\n");
+        contador_rainha++;
+    }while(contador_rainha < RAINHA_CASAS);
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
